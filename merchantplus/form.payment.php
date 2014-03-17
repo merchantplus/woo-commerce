7d2280feae7f0481446915ab8c308883661805d6
Roy Gunawan <gunawan.roy@gmail.com>
2014-03-17 14:32:30 +0700

    Delimiter change
    Using pipe instead of comma

--- merchantplus/form.payment.php
+++ merchantplus/form.payment.php
@@ -23,15 +23,15 @@
 <p class="form-row form-row-first">
     <label>Expiration Month <span class="required">*</span></label>
     <select name="billing_expdatemonth">
-        <option value=1>01</option>
-        <option value=2>02</option>
-        <option value=3>03</option>
-        <option value=4>04</option>
-        <option value=5>05</option>
-        <option value=6>06</option>
-        <option value=7>07</option>
-        <option value=8>08</option>
-        <option value=9>09</option>
+        <option value=01>01</option>
+        <option value=02>02</option>
+        <option value=03>03</option>
+        <option value=04>04</option>
+        <option value=05>05</option>
+        <option value=06>06</option>
+        <option value=07>07</option>
+        <option value=08>08</option>
+        <option value=09>09</option>
         <option value=10>10</option>
         <option value=11>11</option>
         <option value=12>12</option>
@@ -42,8 +42,7 @@
     <select name="billing_expdateyear">
 <?php
     $today = (int)date('Y', time());
-    for($i = 0; $i < 8; $i++)
-    {
+    for($i = 0; $i < 8; $i++){
 ?>
         <option value="<?php echo $today; ?>"><?php echo $today; ?></option>
 <?php
